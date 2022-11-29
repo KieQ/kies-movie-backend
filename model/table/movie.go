57e5e3b7c4fc11172096b7e7d37de8bce47456b9
sunbo.kie <sunbo.kie@bytedance.com>
2022-11-29 13:04:10 +0800

    [ADD] movie sql

--- model/table/movie.go
+++ model/table/movie.go
@@ -3,17 +3,17 @@ package table
 import "time"
 
 type Movie struct {
-	ID         int64     `gorm:"column:id" json:"id"`
-	Name       string    `gorm:"column:name" json:"name"`
-	MagnetLink string    `gorm:"column:magnet_link" json:"magnet_link"`
-	Size       int64     `gorm:"column:size" json:"size"`
-	Location   string    `gorm:"column:location" json:"location"`
-	Profile    string    `gorm:"column:profile" json:"profile"`
-	UserID     string    `gorm:"column:user_id" json:"user_id"`
-	IsPrivate  int32     `gorm:"column:is_private" json:"is_private"`
-	Tags       string    `gorm:"column:tags" json:"tags"`
-	CreateTime time.Time `gorm:"column:create_time" json:"create_time"`
-	UpdateTime time.Time `gorm:"column:update_time" json:"update_time"`
+	ID          int64     `gorm:"column:id" json:"id"`
+	Name        string    `gorm:"column:name" json:"name"`
+	MagnetLink  string    `gorm:"column:magnet_link" json:"magnet_link"`
+	Size        int64     `gorm:"column:size" json:"size"`
+	Location    string    `gorm:"column:location" json:"location"`
+	Profile     string    `gorm:"column:profile" json:"profile"`
+	UserAccount string    `gorm:"column:user_account" json:"user_account"`
+	IsPrivate   bool      `gorm:"column:is_private" json:"is_private"`
+	Tags        string    `gorm:"column:tags" json:"tags"`
+	CreateTime  time.Time `gorm:"column:create_time" json:"create_time"`
+	UpdateTime  time.Time `gorm:"column:update_time" json:"update_time"`
 }
 
 func (m *Movie) Table() string {
